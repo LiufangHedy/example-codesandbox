d3a3967689e65db0171a22110604686c0f7503a9
LiufangHedy <44257847+LiufangHedy@users.noreply.github.com>
2022-12-02 04:22:50 +0800

    add click button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  let [msg, setMsg] = useState("");
+  function clickHandler() {
+    if (!msg) {
+      setMsg("click");
+    } else {
+      setMsg(msg + "  click");
+    }
+  }
   return (
     <div className="App">
       <h1>Hello there</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={clickHandler}> Click Me </button>
+      <p> {msg} hi </p>
     </div>
   );
 }
